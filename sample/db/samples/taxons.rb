847fe0dd87f199d1c943050dd3bb60a217d1c363
Thomas Chen <foxnewsnetwork@gmail.com>
2013-09-18 12:08:02 -0700

    removed space

--- sample/db/samples/taxons.rb
+++ sample/db/samples/taxons.rb
@@ -123,7 +123,6 @@ taxons = [
 taxons.each do |taxon_attrs|
   if taxon_attrs[:parent]
     taxon_attrs[:parent] = Spree::Taxon.find_by_name!(taxon_attrs[:parent])
-    puts taxon_attrs
     Spree::Taxon.create!(taxon_attrs)
   end
 end
