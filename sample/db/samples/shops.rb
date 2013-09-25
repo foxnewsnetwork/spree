66db1eb2f758763e445f747ed41e7d2da4a74066
Thomas Chen <foxnewsnetwork@gmail.com>
2013-09-25 12:28:03 -0700

    comma

--- sample/db/samples/shops.rb
+++ sample/db/samples/shops.rb
@@ -4,6 +4,6 @@ Spree::Sample.load_sample("addresses")
 user = Spree::User.find_by_email! "admin@thomaschen.co"
 address = Spree::Address.find_by_nickname! "Admin Shop"
 user.shop.create!(
-  name: "Admin Shop"
+  name: "Admin Shop",
   email: "shop@thomaschen.co",
   address: address)
