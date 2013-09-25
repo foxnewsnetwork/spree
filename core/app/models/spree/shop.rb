f58ee7d7eb8ec5fc8e5d23221bf652982a7cc0f4
Thomas Chen <foxnewsnetwork@gmail.com>
2013-09-25 12:34:15 -0700

    put in address dependence

--- core/app/models/spree/shop.rb
+++ core/app/models/spree/shop.rb
@@ -1,6 +1,7 @@
 module Spree
   class Shop < ActiveRecord::Base
     belongs_to :user, class_name: 'Spree::User'
+    belongs_to :address, class_name: 'Spree::Address'
     has_many :listings, class_name: 'Spree::Listing'
 
   end
